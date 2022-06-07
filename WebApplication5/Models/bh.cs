117201b56c4f8c6394be72844f4020dabca4838c
Nihed12 <mosbahinihed@gmail.com>
2022-06-07 22:47:44 +0100

    first commit

---
+++ WebApplication5/Models/bh.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class bh
+    {
+        public List<entretien> entretien { get; set; }
+        public List<clientdetails> client { get; set; }
+    }
+}
---
+++ WebApplication5/Models/entretien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class entretien
+    {
+
+        
+
+        public int id_typentr { get; set; }
+        public string libellentr { get; set; }
+        public string action_propose { get; set; }
+        public string description { get; set; }
+
+    }
+
+}
